b2fb6c10298d1553e168c60bd0f0038b7451db40
FabCode67 <mwanafunzifabrice@gmail.com>
2024-09-18 10:37:57 +0200

    agent layout

--- src/components/dashboard/admin/CardWrapper.tsx
+++ src/components/dashboard/admin/CardWrapper.tsx
@@ -3,22 +3,35 @@
 import {
     Card
 } from "@/components/ui/card";
-import { FaMoneyBill } from "react-icons/fa";
+import React from "react";
 
 
 interface DashboardCardWrapperProps {
+    header: string
+    amount: string
+    perMonth: string
+    icon: React.ReactNode
 }
 export const DashboardCardWrapper = ({
-
+    header,
+    amount,
+    perMonth,
+    icon
 }: DashboardCardWrapperProps) => {
     return (
         <Card className="shadow-md text-sm border border-gray-300 flex p-4 flex-col text-gray-700">
             <div className="flex justify-between">
-                <p className="">Header 1</p>
-                <FaMoneyBill className="flex my-auto" />
+                <p className="">
+                    {header}
+                </p>
+                {icon}
             </div>
-            <p className="font-extrabold text-lg mt-4">$134353</p>
-            <p className=" text-gray-700 text-sm">+134353 per month</p>
+            <p className="font-extrabold text-lg mt-4">
+                {amount}
+            </p>
+            <p className=" text-gray-700 text-sm">
+                {perMonth}
+            </p>
         </Card>
     )
 }
